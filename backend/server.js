ce1108654effe5e56ddef742aadfe8b09335b13c
Sebastian Cruz <sebalox92@gmail.com>
2026-07-12 21:37:38 -0400

    front end build

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
+app.use(express.static("../frontend/dist"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 
